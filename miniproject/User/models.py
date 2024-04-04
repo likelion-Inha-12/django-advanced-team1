04a566671e990cbc42cd791fba9fff63f51e92e6
bmh7190 <bmh7190@naver.com>
2024-04-04 23:50:05 +0900

    models.py에 name 추가

--- miniproject/User/models.py
+++ miniproject/User/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class User(models.Model):
     email = models.EmailField(max_length=200)
     password = models.CharField(max_length = 200, default="0000")
+    name =  models.CharField(max_length=50, default="user")
     is_leader = models.BooleanField(default=False)
     hearts = models.IntegerField(default=0)
