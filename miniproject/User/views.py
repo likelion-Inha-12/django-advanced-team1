172f37d344a7d63f0ca98ef596869ab2f3162ae7
minkyeong <haileyre000b@naver.com>
2024-04-04 20:27:34 +0900

    api 7 완성 - 민경

--- miniproject/User/views.py
+++ miniproject/User/views.py
@@ -51,7 +51,6 @@ def delete_user(request, id):
 
 #api6 대표임명_민경
 #회원 이름의 경우 user.email로 구현
-
 def appoint_user(request, id):
     if request.method == 'POST':
         user = get_object_or_404(User, pk=id) #user 존재하지 않으면 404 에러 반환
@@ -72,3 +71,18 @@ def appoint_user(request, id):
     
     else:
         return JsonResponse({'message': 'POST 요청만 허용됩니다.'})
+    
+
+#api7 모든정보조회_민경
+def list_all(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        total_hearts = sum(user.hearts for user in users)
+        data = {
+            "message" : "모든 회원들의 정보 (회원 수, 총 하트수) 입니다.",
+            "member_count": users.count(),
+            "total_hearts": total_hearts
+        }
+        return JsonResponse(data)
+    else:
+        return JsonResponse({'message': 'GET 요청만 허용됩니다.'})
